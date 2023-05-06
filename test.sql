8d44bf721e20720009f873c94ed0ef5af7d61faf
Konstantin Bezgatsev <gelandewagen1@gmail.com>
2023-05-06 15:11:28 +0200

    konstantin update

--- test.sql
+++ test.sql
@@ -1,1 +1,6 @@
-Here need some code...
+SELECT 
+	OrderID as ORDER_NUM, 
+	Sum(Products.Price*Quantity) as TOTAL  
+FROM OrderDetails
+JOIN Products ON OrderDetails.ProductID=Products.ProductID
+GROUP BY OrderID
