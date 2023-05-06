c88259bee26a5c9200ef1117dcc9bb7da8d154df
Konstantin Bezgatsev <gelandewagen1@gmail.com>
2023-05-06 15:33:17 +0200

    add comment task

--- test.sql
+++ test.sql
@@ -1,6 +1,6 @@
-SELECT 
-	OrderID as ORDER_NUM, 
-	Sum(Products.Price*Quantity) as TOTAL  
-FROM OrderDetails
-JOIN Products ON OrderDetails.ProductID=Products.ProductID
-GROUP BY OrderID
+SELECT --выбрать
+	OrderID as ORDER_NUM, --поле айди заказа отобразить как номер заказа
+	Sum(Products.Price*Quantity) as TOTAL  --перемножить цену на количество и посчитать сумму
+FROM OrderDetails --из деталей заказов
+JOIN Products ON OrderDetails.ProductID=Products.ProductID --объединие двух таблиц
+GROUP BY OrderID --сгруппировать
